7869aae6e2235b4cd52af4ec0cd711ff1d174e68
Yogesh Choudhary <yogesh.choudhary@neosoftmail.com>
2023-09-15 19:23:49 +0530

    update key

--- src/main/java/com/methodsecurity/service/JwtService.java
+++ src/main/java/com/methodsecurity/service/JwtService.java
@@ -20,7 +20,9 @@ import java.util.function.Function;
 @Component
 public class JwtService {
 
-    public static final String SECRET_KEY = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    public String getSecretKey() {
+        return "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    }
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -54,9 +56,9 @@ public class JwtService {
     }
 
 
-    public String generateToken(String userName){
-        Map<String,Object> claims=new HashMap<>();
-        return createToken(claims,userName);
+    public String generateToken(String userName) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userName);
     }
 
     private String createToken(Map<String, Object> claims, String userName) {
@@ -64,12 +66,12 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
     private Key getSignKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(getSecretKey());
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
